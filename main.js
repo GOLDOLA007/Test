d60a36bf6cce3375ecbda69cb96c88e2a3e5c311
Kaua <kauaoliveira18062007@gmail.com>
2026-06-19 13:36:36 -0300

    feat: update testAPI to be an asynchronous function

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-const urls = ['http://localhost:8080/users/status-javaconfig'];
-
 setInterval(testAPI, 60000); // Call testAPI every 1min
 let i =0;
-function testAPI() {
-    fetch(urls[0])
+async function testAPI() {
+    await fetch("http://localhost:8080/auth/status-javaconfig")
     .then(response => response.text())
-        //.then(text => console.log(text))
         .then(text => document.getElementById('response').textContent = text)
         .then(text => document.getElementById('counter').textContent = 'API called (1min intervals): ' + i + ' times')
         .catch(error => document.getElementById('response').textContent = 'Error: ' + error);    

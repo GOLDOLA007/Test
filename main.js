aefd72ae215bd0f6e4903bc08805f0f78bc32959
Kaua <kauaoliveira18062007@gmail.com>
2026-06-21 15:13:59 -0300

    fix: update fetch URL in the testAPI function

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ async function registerAPI(){
 setInterval(testAPI, 60000); // Call testAPI every 1min
 let i =0;
 async function testAPI() {
-    await fetch("http://localhost:8080/auth/status-javaconfig")
+    await fetch("http://localhost:8080/auth/status")
     .then(response => response.text())
         .then(text => document.getElementById('response').textContent = text)
         .then(text => document.getElementById('counter').textContent = 'API called (1min intervals): ' + i + ' times')
